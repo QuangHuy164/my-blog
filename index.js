647a52a564c627229216bbc35033dbbbb1375f32
unknown <nghuy164@gmail.com>
2024-05-05 04:42:12 +0300

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('Hello');
+console.log('I like kebab');
+
+window.alert('alert');
+window.alert('I like kebab');
+
+document.getElementById('myH1').textContent = 'Hello';
+document.getElementById('myP').textContent = 'I like kebab';
