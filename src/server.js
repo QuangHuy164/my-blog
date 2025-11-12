f3f8c10742fc37955a4fc57071560ec6ade104b5
QuangHuy164 <nghuy164@gmail.com>
2025-11-12 15:32:45 +0200

    update

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const morgan = require('morgan')
 const app = express()
-const exphbs = require('express-handlebars')
+const {engine} = require('express-handlebars')
 const port = 3001
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -11,14 +11,14 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(morgan('combined'))
 
 // Template engine
-app.engine('hbs', exphbs.engine({
+app.engine('hbs', engine({
   extname: '.hbs',
   defaultLayout: 'main',
-  layoutsDir: path.join(__dirname, 'resources/views/layouts'),
-  partialsDir: path.join(__dirname, 'resources/views/partials')
+  layoutsDir: path.join(__dirname, 'resources','views', 'layouts'),
+  partialsDir: path.join(__dirname, 'resources', 'views','partials')
 }))
 app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, '/resources/views'))
+app.set('views', path.join(__dirname, 'resources', 'views'))
 
 
 app.get('/', (req, res) => {
