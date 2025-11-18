574dd903f789b9140d63f2e6e2888b6cfa01a54b
QuangHuy164 <nghuy164@gmail.com>
2025-11-18 18:46:23 +0200

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,9 @@
 const newsRouter = require('./news')
 
 const route = (app) => {
-    app.get('/', (req, res) => {
-  res.render('home')
-})
 
-app.use('/news', newsRouter)
+  app.use('/news', newsRouter)
 
-app.get('/search', (req, res) => {
-  console.log(req.query)
-  res.render('search')
-})
-
-app.post('/search', (req, res) => {
-  res.send('')
-})
 }
 
 module.exports = route
