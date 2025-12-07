495154b1535e347d96198e606683558a038b57b8
QuangHuy164 <nghuy164@gmail.com>
2025-12-08 01:11:24 +0200

    update

---
+++ src/app/controllers/MeController.js
@@ -0,0 +1,26 @@
+const BlogPost = require('../models/BlogPost');
+
+class SiteController {
+    // [GET] /
+    index(req, res, next) {
+        // lean() returns a JS obj instead of a Mongoose docs
+        BlogPost.find({}).lean()
+        .then(blogs => res.render('home', {blogs}))
+        .catch(next)
+    }
+    // index(req, res, next) {
+    //     BlogPost.find({})
+    //         .then(blogs => {
+    //         blogs = blogs.map(blog => blog.toObject())
+    //         res.render('home', {blogs})})
+    //         .catch(next);
+    // }
+
+    // [GET] /search
+
+    search(req, res) {
+        res.render('search');
+    }
+}
+
+module.exports = new SiteController();
