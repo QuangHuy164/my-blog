3f772c87a613c8d949a46f9217a8a6c0fb06b063
QuangHuy164 <nghuy164@gmail.com>
2026-01-13 19:23:53 +0200

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
     try {
+         mongoose.set('strictQuery', false);
         await mongoose.connect('mongodb://localhost:27017/myblog_dev');
         console.log('success');
     } catch (error) {
